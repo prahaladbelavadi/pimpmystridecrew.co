037c6f3b17b83b6e175a22e0dd982af484090ae3
Prahalad Belavadi <test@ALIPL0232.local>
2020-04-29 09:03:59 +0530

    Add crew component template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CrewComponent } from './crew/crew.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent },
+  { path: 'crew', component: CrewComponent },
+  { path: '**', component: LandingPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CrewComponent } from './crew/crew.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    CrewComponent
   ],
   imports: [
     BrowserModule,
